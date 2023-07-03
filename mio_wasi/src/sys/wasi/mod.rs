a72e94a43e320d39c6c8796bbe89fb3831fc3962
dm4 <dm4@secondstate.io>
2023-07-03 13:59:27 +0800

    Revert "Use local tokio_wasi and mio_wasi to downgrade wasmedge_wasi_socket to 0.4.3"
    This reverts commit ea2cf01a165525aa3865fa9f4a2fe0f468efdcff.
    
    Signed-off-by: dm4 <dm4@secondstate.io>

--- mio_wasi/src/sys/wasi/mod.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(not(feature = "wasmedge"))]
-mod wasmtime;
-#[cfg(not(feature = "wasmedge"))]
-pub use self::wasmtime::*;
-#[cfg(feature = "wasmedge")]
-mod wasmedge;
-#[cfg(feature = "wasmedge")]
-pub use self::wasmedge::*;
